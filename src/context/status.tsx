5106728b35977b97235a9fe23ba97900b30182f1
Melksedeque <freelancer@melksedeque.com.br>
2025-03-14 18:10:46 -0300

    Correção na exportação do contexto de Status

--- src/context/status.tsx
+++ src/context/status.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 const valorPadrao = "Você ainda não selecionou nenhuma raça! :(";
-const statusContext = React.createContext(valorPadrao);
+const StatusContext = React.createContext(valorPadrao);
 
-export default statusContext;
+export default StatusContext;
