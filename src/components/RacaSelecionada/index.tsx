0ba3a8a19ea078d54616e779a5a20fcf528873b8
Melksedeque <freelancer@melksedeque.com.br>
2025-03-15 16:44:57 -0300

    Correção do uso de const RacaSelecionada ao inves de class

--- src/components/RacaSelecionada/index.tsx
+++ src/components/RacaSelecionada/index.tsx
@@ -1,7 +1,5 @@
 import styles from "./RacaSelecionada.module.css";
 
-import React from "react";
-
 interface RacaSelecionadaProps {
   raca: {
     name?: string;
@@ -13,33 +11,31 @@ interface RacaSelecionadaProps {
   };
 }
 
-class RacaSelecionada extends React.Component<RacaSelecionadaProps> {
-  render() {
-    const { raca } = this.props;
-    if (!raca.name) {
-      return null;
-    }
-    return (
-      <>
-        <div className={styles.raca_selecionada}>
-          <img 
-            src={raca.imagem} 
-            alt={raca.name}
-            onError={(e) => {
-              e.currentTarget.src = 'https://via.placeholder.com/400x300?text=Imagem+não+encontrada';
-            }}
-          />
-        </div>
-        <div className={styles.info}>
-          <h1>{raca.name}</h1>
-          {raca.bred_for && <p>Criado para: {raca.bred_for}</p>}
-          {raca.breed_group && <p>Grupo: {raca.breed_group}</p>}
-          {raca.life_span && <p>Tempo médio de vida: {raca.life_span}</p>}
-          {raca.temperament && <p>Temperamento: {raca.temperament}</p>}
-        </div>
-      </>
-    );
+const RacaSelecionada = ({ raca }: RacaSelecionadaProps) => {
+  if (!raca.name) {
+    return null;
   }
-}
+  
+  return (
+    <>
+      <div className={styles.raca_selecionada}>
+        <img 
+          src={raca.imagem} 
+          alt={raca.name}
+          onError={(e) => {
+            e.currentTarget.src = 'https://via.placeholder.com/400x300?text=Imagem+não+encontrada';
+          }}
+        />
+      </div>
+      <div className={styles.info}>
+        <h1>{raca.name}</h1>
+        {raca.bred_for && <p>Criado para: {raca.bred_for}</p>}
+        {raca.breed_group && <p>Grupo: {raca.breed_group}</p>}
+        {raca.life_span && <p>Tempo médio de vida: {raca.life_span}</p>}
+        {raca.temperament && <p>Temperamento: {raca.temperament}</p>}
+      </div>
+    </>
+  );
+};
 
 export default RacaSelecionada;
