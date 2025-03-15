1588630059b11b337c715e916448f0c8ee67ce83
Melksedeque <freelancer@melksedeque.com.br>
2025-03-15 16:58:27 -0300

    Adição de classes para estilização. Reestruturação de componente

--- src/components/RacaSelecionada/index.tsx
+++ src/components/RacaSelecionada/index.tsx
@@ -15,26 +15,30 @@ const RacaSelecionada = ({ raca }: RacaSelecionadaProps) => {
   if (!raca.name) {
     return null;
   }
-  
+
   return (
-    <>
-      <div className={styles.raca_selecionada}>
-        <img 
-          src={raca.imagem} 
+    <div className={styles.raca_selecionada}>
+      <figure className={styles.imagem_raca}>
+        <img
+          src={raca.imagem}
           alt={raca.name}
           onError={(e) => {
-            e.currentTarget.src = 'https://via.placeholder.com/400x300?text=Imagem+não+encontrada';
+            e.currentTarget.src =
+              "https://via.placeholder.com/400x300?text=Imagem+não+encontrada";
           }}
         />
-      </div>
+        <figcaption sr-only hidden>
+          <em>Imagem de {raca.name}</em>
+        </figcaption>
+      </figure>
       <div className={styles.info}>
-        <h1>{raca.name}</h1>
+        <h1 className={styles.name}>{raca.name}</h1>
         {raca.bred_for && <p>Criado para: {raca.bred_for}</p>}
         {raca.breed_group && <p>Grupo: {raca.breed_group}</p>}
         {raca.life_span && <p>Tempo médio de vida: {raca.life_span}</p>}
         {raca.temperament && <p>Temperamento: {raca.temperament}</p>}
       </div>
-    </>
+    </div>
   );
 };
 
