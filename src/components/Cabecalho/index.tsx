dee6993e10f4c455d08bca72eda6309b4753f177
Melksedeque <freelancer@melksedeque.com.br>
2025-03-15 18:52:41 -0300

    Adiçã ode descritivo ao cabeçalho

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -5,6 +5,10 @@ const Cabecalho: React.FC = () => {
   return (
     <header className={styles.cabecalho}>
       <h1 className={styles.cabecalho__titulo}>Dogpedia</h1>
+      <p>
+        Clique em um nome e te daremos informações úteis sobre a raça e uma
+        imagem bem bonita
+      </p>
       <Status />
     </header>
   );
