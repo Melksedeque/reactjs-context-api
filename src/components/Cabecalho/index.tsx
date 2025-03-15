d4fddc6c3f5179c6ab778ce963fbc4ec98fc4a3b
Melksedeque <freelancer@melksedeque.com.br>
2025-03-15 17:10:25 -0300

    Limpeza de código não utilizado e remoção de prop drilling

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,21 +1,13 @@
 import Status from "../Status";
 import styles from "./Cabecalho.module.css";
 
-import React from "react";
-
-interface CabecalhoProps {
-  status: string;
-}
-
-class Cabecalho extends React.Component<CabecalhoProps> {
-  render() {
-    return (
-      <header className={styles.cabecalho}>
-        <h1 className={styles.cabecalho__titulo}>Dogpedia</h1>
-        <Status status={this.props.status} />
-      </header>
-    );
-  }
-}
+const Cabecalho = () => {
+  return (
+    <header className={styles.cabecalho}>
+      <h1 className={styles.cabecalho__titulo}>Dogpedia</h1>
+      <Status />
+    </header>
+  );
+};
 
 export default Cabecalho;
